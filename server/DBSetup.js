de5c3f21f0126d5fe507fa44d963e93069be2fd4
Jaredhw <59721762+Jaredhw@users.noreply.github.com>
2020-11-18 06:45:18 -0800

    Update "Type" and Organization tables to match client documentation

--- server/DBSetup.js
+++ server/DBSetup.js
@@ -69,11 +69,13 @@ create(
   ')'
 );
 
-// TODO: Update this table based on the docs
+// Updated to match the requirements docs Nov 18th, 2020
 insert('FamilyStatusType(name)', [
   ['Single'],
-  ['Married'],
-  ['Divorced'],
+  ['Couple'],
+  ['Couple with Children'],
+  ['Single Parent'],
+  ['Existing Group'],
 ]);
 
 create(
@@ -85,7 +87,6 @@ create(
   ')'
 );
 
-// TODO: Update this table based on the docs
 insert('AgeGroupType(name, minAge, maxAge)', [
   ['Baby', 0, 3],
   ['Child', 4, 12],
@@ -131,22 +132,36 @@ create(
 );
 
 // Creates Organization related tables.
+// Tables based on "registration questions oct 2020 UBCO"
 create(
   'Organization (' +
     'id INT AUTO_INCREMENT PRIMARY KEY,' +
+    //----- Information for Internal Verification Purposes -----
     // Only verified organizations can make listings
     'verified BOOLEAN,' +
-    // This is the public information made available for this organization.
-    'name VARCHAR(100),' +
-    'website VARCHAR(100),' +
-    'phone VARCHAR(20),' +
-    'email VARCHAR(100),' +
-    'streetAddress VARCHAR (200),' +
-    'postalCode VARCHAR(10),' +
-    'incorporated BOOLEAN' +
+    'registrationDate DATE,' +
+    // If incorporated is true, include the other information, if not leave it null
+    'incorporated BOOLEAN,' +
+    'incorporatedName VARCHAR(300),' +
+    'incorporatedOwners VARCHAR(300),' +
+    'contactFirstName VARCHAR(300),' +
+    'contactLastName VARCHAR(300),' +
+    'contactPhone VARCHAR(300),' +
     // TODO: Setup login with salted and hashed information
     // 'loginName VARCHAR(200),' +
     // 'loginPassword VARCHAR(1000),' +
+    
+    //----- Publically Displayed Information (All have "organization" prefix) -----
+    'organizationName VARCHAR(100),' +
+    'organizationWebsite VARCHAR(100),' + // Website is not required
+    'organizationLogoURL VARCHAR(100),' +
+    'organizationMainPhone VARCHAR(20),' +
+    'organizationAltPhone VARCHAR(20),' +
+    'organizationEmail VARCHAR(100),' +
+    // Address Information, Only postalCode is required
+    'organizationStreetAddress VARCHAR (200),' +
+    'organizationMailingAddress VARCHAR(200),' +
+    'organizationPostalCode VARCHAR(10)' +
   ')'
 );
 
@@ -159,11 +174,18 @@ create(
   ')'
 );
 
-// TODO: Update this table based on the docs
+// Categories takes fron "Final Sub Category Ad Questions Oct 14"
 insert('CategoryType(name, paymentRequired)', [
-  ['Non-Profit', false],
-  ['For-profit', true],
-  ['Other', false],
+  // These listing categories require payment (paymentRequired == true)
+  ['Rental', true],
+  ['House & Yard Services', true],
+  ['Legal & Sales', true],
+  ['Classes, Clubs, and Events', true],
+  // These listin' categories are free (paymentRequired == false)
+  ['Cohousing, Co-ops, Intergenerational, and Planned Neighborhoods', false],
+  ['Home Share Facilitation & Matching Services, and Planned Neighborhoods', false],
+  ['Government & Non-Profit / Shared Living Supports and Services ', false],
+  ['Members with Homes to Share', false],
 ]);
 
 create(
@@ -186,22 +208,22 @@ create(
 
 
 // Inserts example data for testing.
-insert('Organization(verified, name, website, phone, email, streetAddress, postalCode, incorporated)', [
-  [true,'Larry\'s Lizard Shack', 'larryzlizards.com', '250-555-1234', 'larry@larryzlizards.com', '1111 Alabama Way', 'V1X3P6', true],
-  [true,'The Grass Assassins', 'grassassassins.com', '250-555-0987', 'contact@grassassassins.com', '1234 Montreal Road, Armstrong, BC', 'V1Z 9P6', true],
-  [true,'Grass B Gone', 'grassbgone.ca', '413-555-1983', 'info@grassbgone.ca', 'Unit 400 1234 Apartment Street, Kelowna, BC, Canada', 'V1Z3PZ', false],
-  [false,'Dog Walking CORP', 'legitdogwalkingcomapany.xyz', '250-555-1111', 'walking@dogCorp.xyz', 'PO BOX 11113, Montreal CANADA', 'T4Xu8x', true],
-  [true,'BC Housing Assistance', 'housingassistance.gov.bc.ca', '250-555-1331', 'HousingAssistance@gov.bc.ca', '1234 Parliment Way, Ottowa, Canada, ', 'K1A 0A6', false],
+insert('Organization(verified, organizationName, registrationDate, organizationWebsite, organizationMainPhone, organizationEmail, organizationStreetAddress, organizationPostalCode, incorporated)', [
+  [true,'Larry\'s Lizard Shack', '2010-10-1', 'larryzlizards.com', '250-555-1234', 'larry@larryzlizards.com', '1111 Alabama Way', 'V1X3P6', true],
+  [true,'The Grass Assassins', '2014-7-29', 'grassassassins.com', '250-555-0987', 'contact@grassassassins.com', '1234 Montreal Road, Armstrong, BC', 'V1Z 9P6', true],
+  [true,'Grass B Gone', '2018-3-6', 'grassbgone.ca', '413-555-1983', 'info@grassbgone.ca', 'Unit 400 1234 Apartment Street, Kelowna, BC, Canada', 'V1Z3PZ', false],
+  [false,'Dog Walking CORP', '2020-7-20', 'legitdogwalkingcomapany.xyz', '250-555-1111', 'walking@dogCorp.xyz', 'PO BOX 11113, Montreal CANADA', 'T4Xu8x', true],
+  [true,'BC Housing Assistance', '1957-01-15', 'housingassistance.gov.bc.ca', '250-555-1331', 'HousingAssistance@gov.bc.ca', '1234 Parliment Way, Ottowa, Canada, ', 'K1A 0A6', false],
 ]);
 
 // Value Template
-// [approvalStatus,'Title', 'Website', 'Phone', 'E-Mail', 'Description', 'imageURL', 'categoryID','organizationID'],
+// [approvalStatus,'Title', 'Website', 'Phone', 'E-Mail', 'Description', 'imageURL', categoryID, organizationID],
 insert('Listing(approvalStatus, title, website, phone, email, description, imageURL, categoryID, organizationID)', [
   [true,'Larry\'s Lizard Rental Service', 'larryzlizards.com', '250-555-1234', 'larry@larryzlizards.com', 'Description', 'imageURL', 2,1],
   [true,'Grass Assassins Grass Cutting Service', 'grassassassins.com', '250-555-0987', 'contact@grassassassins.com', 'We will cut your grass for a fair and reasonable price, our specialty is cutting grass so quiet you would never hear it', 'NO IMAGE', 2,2],
   [true,'Grass B Gone Landscaping Service', 'grassbgone.ca', '413-555-1983', 'info@grassbgone.ca', 'Tired of watering the lawn every week? Tired of paying to get your lawn mowed? Contact us about our xeroscaping services, never water again!', '/image/testimage/test.jpg', 2,3],
-  [true,'BC Housing - Housing Placement Service', 'placement.housingassistance.gov.bc.ca', '1-250-555-8000', 'HousingAssistance@gov.bc.ca', 'We\'ll help you find an afforable place to live', '/image/testimage/BCGOVLOGO.png', 1,5],
-  [true,'BC Housing - Lease Assistance Service', 'leasehelp.housingassistance.gov.bc.ca', '1-250-555-8001', 'HousingAssistance@gov.bc.ca', 'We\'ll help you create, mange disputes, and understand lease agreements in BC', '/image/testimage/BCGOVLOGO.png', 1,5],
+  [true,'BC Housing - Housing Placement Service', 'placement.housingassistance.gov.bc.ca', '1-250-555-8000', 'HousingAssistance@gov.bc.ca', 'We\'ll help you find an afforable place to live', '/image/testimage/BCGOVLOGO.png', 7,5],
+  [true,'BC Housing - Lease Assistance Service', 'leasehelp.housingassistance.gov.bc.ca', '1-250-555-8001', 'HousingAssistance@gov.bc.ca', 'We\'ll help you create, mange disputes, and understand lease agreements in BC', '/image/testimage/BCGOVLOGO.png', 7,5],
   [false,'Dog Walking - CHEAP', 'legitdogwalkingcomapany.xyz', '250-555-1111', 'walking@dogCorp.xyz', 'Will walk your dog for cheap, please pay via Monero Money Transfer', 'No Image', 3,2],
 
 ]);

807177edfb7942e41805dc53c7e2e953abf11108
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-12 19:53:47 -0800

    Add initial linting config

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import Main from './Main';
-import NavBar from './NavBar';
-import './App.css';
+import React, { Component } from "react";
+import Main from "./Main";
+import NavBar from "./NavBar";
+import "./App.css";
 
 // Base component of the app in which everything is rendered
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <NavBar />
         <Main />
       </div>

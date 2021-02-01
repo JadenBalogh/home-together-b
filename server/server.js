09aa0d0992ee2426a304b00978475cd31bbddb2c
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-31 16:22:12 -0800

    Add static React serving logic

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import path from 'path';
+const __dirname = path.resolve();
+
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -8,9 +11,14 @@ import authRoutes from './routes/auth-routes.js';
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(3001, () => {
   console.log('Server started on port 3001');
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 searchRoutes(app);

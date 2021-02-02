0b6ffb9d3ce8de1810f11738cadd4df08f8e6fd0
AA666666 <52806201+AA666666@users.noreply.github.com>
2021-02-02 12:21:20 -0800

    update password and account page

--- client/src/views/Settings/Account.js
+++ client/src/views/Settings/Account.js
@@ -2,36 +2,75 @@
  * @Author: AA
  * @Date: 2021-01-28 16:04:34
  * @LastEditors: AA
- * @LastEditTime: 2021-01-29 06:38:22
- * @FilePath: /src/views/Settings/Account.js
+ * @LastEditTime: 2021-01-30 14:54:49
+ * @FilePath: /src/views/Settings/Password.js
  */
 import React from "react";
+
+
+
 // Import plugin resources
 import {
-  Container,
-  Grid,
   Card,
   CardContent,
   CardActions,
-  Typography,
   Button,
+  TextField,
 } from "@material-ui/core";
 
 // This plugin resources
+
+// Import Global Images
+
 
 // Import  dependent resources of this component
 
 export default function Account() {
   return (
-    <Card className="aa-card p-20">
-      <CardContent>
-        <CardContent></CardContent>
-        <CardActions className="text-center">
-          <Button className="m-auto" size="small">
-            <i className="fa fa-smile-o" aria-hidden="true"></i> Coming soon!!!
-          </Button>
-        </CardActions>
-      </CardContent>
+    <Card className="aa-card">
+      <form autoComplete="off">
+        <CardContent>
+          <CardContent>
+           
+            <div className="row">
+              <div className="col-24 mb-20">
+                <TextField
+                  className="w-100"
+                  required
+                  id="oldpassword"
+                  label="Enter current password..."
+                  defaultValue=""
+                />
+              </div>
+              <div className="col-24 mb-20">
+                <TextField
+                  className="w-100"
+                  required
+                  id="address"
+                  label="Enter new email address..."
+                  defaultValue=""
+                />
+              </div>
+              <div className="col-24 mb-20">
+                <TextField
+                  className="w-100"
+                  required
+                  id="mailaddress"
+                  label="Confirm new email address..."
+                  defaultValue=""
+                />
+              </div>
+              
+              
+            </div>
+          </CardContent>
+          <CardActions className="text-center">
+            <Button className="m-auto" variant="contained" color="primary">
+              Save
+            </Button>
+          </CardActions>
+        </CardContent>
+      </form>
     </Card>
   );
 }

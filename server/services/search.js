3db69afdc333898f3eb8649a7868b3b59fbe3499
Jaden Balogh <jadenbalogh@gmail.com>
2021-02-07 18:09:48 -0800

    Update member search route

--- server/services/search.js
+++ server/services/search.js
@@ -8,53 +8,67 @@ function getAgeRanges(ageGroupIds) {
   return dbutils.query(sql, [...ageGroupIds]);
 }
 
-function getMembers(genderIds = [], ageGroupIds = [], familyStatusIds = [], maxMonthlyBudget = 0) {
-  genderIds = Array.from(genderIds);
-  ageGroupIds = Array.from(ageGroupIds);
-  familyStatusIds = Array.from(familyStatusIds);
+function getMembers(filters) {
+  let genderIds = Array.from(filters.genderIds);
+  let ageGroupIds = Array.from(filters.ageGroupIds);
+  let familyStatusIds = Array.from(filters.familyStatusIds);
 
-  var sql =
-    'SELECT \
-      m.id AS id, \
-      m.firstName AS firstName, \
-      m.lastName AS lastName, \
-      g.id AS genderId, \
-      g.name AS gender, \
-      f.id AS familyStatusId, \
-      f.name AS familyStatus, \
-      s.maxMonthlyBudget AS maxMonthlyBudget, \
-      s.birthYear as birthYear \
-    FROM SearchableInfo s \
-    JOIN Member m ON m.id = s.memberID \
-    JOIN GenderType g ON g.id = s.genderId \
-    JOIN FamilyStatusType f ON f.id = s.familyStatusId \
-    WHERE s.maxMonthlyBudget >= ?';
+  var sql = `SELECT
+      m.id AS id,
+      m.firstName AS firstName,
+      m.lastName AS lastName,
+      g.id AS genderId,
+      g.name AS gender,
+      f.id AS familyStatusId,
+      f.name AS familyStatus,
+      s.maxMonthlyBudget AS maxMonthlyBudget,
+      s.birthYear as birthYear
+    FROM SearchableInfo s
+    JOIN Member m ON m.id = s.memberID
+    JOIN GenderType g ON g.id = s.genderId
+    JOIN FamilyStatusType f ON f.id = s.familyStatusId
+    WHERE
+      s.maxMonthlyBudget >= ? AND
+      s.petRestrictions = ? AND
+      s.religionRestrictions = ? AND
+      s.smokingRestrictions = ? AND
+      s.hasHousing = ?`;
 
   return new Promise((resolve) => {
-    dbutils.query(sql, [maxMonthlyBudget]).then((results) => {
-      // Manually do the filtering for the array-based parameters
-      // cause prepared statements suck at dealing with this
-      var year = new Date().getFullYear();
-      getAgeRanges(ageGroupIds).then((ageRanges) => {
-        let filteredResults = results.filter((x) => {
-          let doesGenderMatch = genderIds.length > 0 ? genderIds.includes(x.genderId.toString()) : true;
+    dbutils
+      .query(sql, [
+        filters.maxMonthlyBudget,
+        filters.petRestrictions,
+        filters.religionRestrictions,
+        filters.smokingRestrictions,
+        filters.hasHousing,
+      ])
+      .then((results) => {
+        // Manually do the filtering for the array-based parameters
+        // cause prepared statements suck at dealing with this
+        var year = new Date().getFullYear();
+        getAgeRanges(ageGroupIds).then((ageRanges) => {
+          let filteredResults = results.filter((x) => {
+            let doesGenderMatch = genderIds.length > 0 ? genderIds.includes(x.genderId.toString()) : true;
 
-          let doesFamilyMatch =
-            familyStatusIds.length > 0 ? familyStatusIds.includes(x.familyStatusId.toString()) : true;
+            let doesFamilyMatch =
+              familyStatusIds.length > 0 ? familyStatusIds.includes(x.familyStatusId.toString()) : true;
 
-          let doesAgeMatch =
-            ageRanges.length > 0
-              ? ageRanges.some((ageRange) => {
-                  return year - Number(ageRange.minAge) >= x.birthYear && year - Number(ageRange.maxAge) <= x.birthYear;
-                })
-              : true;
+            let doesAgeMatch =
+              ageRanges.length > 0
+                ? ageRanges.some((ageRange) => {
+                    return (
+                      year - Number(ageRange.minAge) >= x.birthYear && year - Number(ageRange.maxAge) <= x.birthYear
+                    );
+                  })
+                : true;
 
-          return doesGenderMatch && doesFamilyMatch && doesAgeMatch;
+            return doesGenderMatch && doesFamilyMatch && doesAgeMatch;
+          });
+
+          resolve(filteredResults);
         });
-
-        resolve(filteredResults);
       });
-    });
   });
 }
 

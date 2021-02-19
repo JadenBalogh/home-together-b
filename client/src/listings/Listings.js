bcae236ea7d9e52ed58df00641de071c45c72dc5
Jaden Balogh <jadenbalogh@gmail.com>
2021-02-19 03:48:43 -0800

    Update listings back-end and front-end

--- client/src/listings/Listings.js
+++ client/src/listings/Listings.js
@@ -25,7 +25,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 // Search page for members
 function Listings(props) {
   const [listings, setListings] = useState([]);
@@ -42,6 +41,7 @@ function Listings(props) {
     fetch(url + route + params)
       .then((res) => res.json())
       .then((json) => {
+        console.log(json);
         setListings(json);
       });
   }
@@ -64,82 +64,94 @@ function Listings(props) {
   }
 
   const classes = useStyles();
-  // TODO: Update this to connect to the DB, current categories are only placeholders, the proper ones will be pulled from the DB
   return (
     <div className='listings-container'>
-      <Typography component="h1" variant="h5">
+      <Typography component='h1' variant='h5'>
         Find Services:
       </Typography>
       <form className={classes.form} noValidate>
-        <Grid container spacing={3} direction="row" justify="space-evenly" alignItems="flex-end">
+        <Grid container spacing={3} direction='row' justify='space-evenly' alignItems='flex-end'>
           <Grid item xs>
-            <InputLabel>
-              Select a Category:
-            </InputLabel>
-            <Select isClearable id="category" className='listing-select' name={'categoryId'} required value={props.category} onChange={props.handleDropdownChange}>
-              <MenuItem value="1">Rentals</MenuItem>
-              <MenuItem value="2">Home and Yard Services</MenuItem>
-              <MenuItem value="3">Legal and Sales Services</MenuItem>
-              <MenuItem value="4">Classes, Clubs, and Events</MenuItem>
+            <InputLabel>Select a Category:</InputLabel>
+            <Select
+              id='category'
+              defaultValue=''
+              className='listing-select'
+              name={'categoryId'}
+              required
+              value={props.category}
+              onChange={props.handleDropdownChange}
+            >
+              <MenuItem value='1'>Rentals</MenuItem>
+              <MenuItem value='2'>Home and Yard Services</MenuItem>
+              <MenuItem value='3'>Legal and Sales Services</MenuItem>
+              <MenuItem value='4'>Classes, Clubs, and Events</MenuItem>
             </Select>
           </Grid>
           <Grid item xs>
-            <InputLabel>
-              Select a Sub-category:
-            </InputLabel>
-            <Select isClearable id="sub-category" defaultValue="1" className='listing-select' name={'sub-categoryId'} required value={props.category} onChange={props.handleDropdownChange}>
-              <MenuItem value="1">All Sub-categories</MenuItem>
-              <MenuItem value="2">Sub 2</MenuItem>
-              <MenuItem value="3">Sub 3</MenuItem>
-              <MenuItem value="4">Sub 4</MenuItem>
+            <InputLabel>Select a Sub-category:</InputLabel>
+            <Select
+              id='sub-category'
+              defaultValue='1'
+              className='listing-select'
+              name={'sub-categoryId'}
+              required
+              value={props.category}
+              onChange={props.handleDropdownChange}
+            >
+              <MenuItem value='1'>All Sub-categories</MenuItem>
+              <MenuItem value='2'>Sub 2</MenuItem>
+              <MenuItem value='3'>Sub 3</MenuItem>
+              <MenuItem value='4'>Sub 4</MenuItem>
             </Select>
           </Grid>
         </Grid>
-        <Grid container spacing={2} justify="center" alignItems="flex-start" wrap="wrap">
+        <Grid container spacing={2} justify='center' alignItems='flex-start' wrap='wrap'>
           <Grid item xs={3}>
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               fullWidth
-              id="titleText"
-              label="Listing Title"
-              name="listingTitle"
-              placeholder="Example"
+              id='titleText'
+              label='Listing Title'
+              name='listingTitle'
+              placeholder='Example'
               autoFocus
             />
           </Grid>
           <Grid item xs={3}>
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               fullWidth
-              id="postalCode"
-              label="Postal Code"
-              name="postalCode"
-              placeholder="V1V 1V1"
+              id='postalCode'
+              label='Postal Code'
+              name='postalCode'
+              placeholder='V1V 1V1'
               autoFocus
             />
           </Grid>
           <Grid item xs={3}>
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               fullWidth
-              id="minimumRating"
-              label="Minimum Rating"
-              name="minimumRating"
-              placeholder="0.0 to 5.0"
+              id='minimumRating'
+              label='Minimum Rating'
+              name='minimumRating'
+              placeholder='0.0 to 5.0'
               autoFocus
-            /></Grid>
+            />
+          </Grid>
           <Grid item xs={3}>
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               fullWidth
-              id="maximumRating"
-              label="Maximum Rating"
-              name="maximumRating"
-              placeholder="0.0 to 5.0"
+              id='maximumRating'
+              label='Maximum Rating'
+              name='maximumRating'
+              placeholder='0.0 to 5.0'
               autoFocus
             />
           </Grid>
@@ -147,8 +159,8 @@ function Listings(props) {
       </form>
       <SearchClearSnackbar SearchClearSnackbar={SearchClearSnackbar}></SearchClearSnackbar>
       <ListingList listings={listings}></ListingList>
-      <Grid container direction="column" justify="center" alignItems="center">
-      <PaginationControlled PaginationControlled={PaginationControlled}></PaginationControlled>
+      <Grid container direction='column' justify='center' alignItems='center'>
+        <PaginationControlled PaginationControlled={PaginationControlled}></PaginationControlled>
       </Grid>
     </div>
   );

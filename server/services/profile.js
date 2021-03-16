3c7df748dd854531c934fbfff786fea26bd5d263
a1v1n1 <qwertwatt@gmail.com>
2021-03-16 13:33:16 -0700

    Fix select

--- server/services/profile.js
+++ server/services/profile.js
@@ -127,7 +127,7 @@ const SQL_UPDATE_ORGANIZATION = `
     national,
     organizationStreetAddress,
     organizationMailingAddress,
-    organizationPostalCode,
+    organizationPostalCode
   WHERE id = ?
 `;
 
